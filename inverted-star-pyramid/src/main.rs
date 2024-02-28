1dc3ecbb83e956c7478f4016f7ff6d26f3795325
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-02-28 22:33:51 +0530

    Added Inverted Star Pyramid pattern

---
+++ inverted-star-pyramid/src/main.rs
@@ -0,0 +1,12 @@
+fn main()   {
+    let height=5;
+    for i in 0..height{
+        for _ in 0..i{
+            print!(" ");
+        }
+        for j in 0..(2*height-2*i-1){
+            print!("*");
+        }
+        println!();
+    }
+}
