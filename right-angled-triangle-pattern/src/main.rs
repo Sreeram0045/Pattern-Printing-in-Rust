c1314a6c0d93b7ac98fd3f40994c1b3e1b402bbf
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-02-29 20:01:25 +0530

    Added right angled triangle pattern

--- right-angled-triangle-pattern/src/main.rs
+++ right-angled-triangle-pattern/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main()   {
+    let height=5;
+    for i in 0..height{
+        let num="* ".repeat(i+1);
+        println!("{}",num);
+    }
 }
+
+// repeat function adds the string "* " i+1 to the variable num, which is then printed later
+
+// output:-
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
