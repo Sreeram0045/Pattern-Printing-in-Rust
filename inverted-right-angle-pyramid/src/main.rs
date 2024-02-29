9f9079a34297e0dd9ad113bce816cf2316f294c1
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-02-29 20:00:06 +0530

    Added inverted right angled pyramid

--- inverted-right-angle-pyramid/src/main.rs
+++ inverted-right-angle-pyramid/src/main.rs
@@ -1,7 +1,16 @@
+
 fn main() {
     let height = 7;
     for i in (0..height).rev() {
-        let stars = "*".repeat(i + 1);
+        let stars = "* ".repeat(i + 1);
         println!("{}", stars);
     }
 }
+
+// output:-
+
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
